796b50dd6d82464de3dd21f56ee14cd55529ffe8
zukas <julius.zukauskas@gmx.com>
2016-10-19 18:15:31 +0200

    prof fix

--- engine/index.js
+++ engine/index.js
@@ -8,6 +8,7 @@ var check 	= require("./validate"),
 	orders 	= require("./orders"),
 	gallery = require("./gallery"),
 	contact = require("./contact"),
+	// lang 	= require("./language"),
 	config 	= require("../config")(),
 	url 	= require('url'),
 	sharp 	= require('sharp'),
@@ -58,354 +59,282 @@ check.installMod("firstCap", function (data) {
 })
 
 exports.index = function(req, res){
-	prof("index", function (end) {
-		var renderFile = req.session.render || (req.device.type == "desktop" ? "desktop.html" : "mobile.html");
-		var renderData = req.session.renderData || { admin: req.session.admin };
-		req.session.render = null;
-		req.session.renderData = null;
 
-		if(renderData && config.debug) {
-			renderData.debug = true;
-		} 
+	var renderFile = req.session.render || (req.device.type == "desktop" ? "desktop.html" : "mobile.html");
+	var renderData = req.session.renderData || {};
+	var sessionLang = req.session.lang || "pl";
+	req.session.render = null;
+	req.session.renderData = null;
+
+	if(renderData && config.debug) {
+		renderData.debug = true;
+	} 
+
+	renderData.admin = req.session.admin;
+	// renderData.lang = lang.fetch(sessionLang);
+	
+	fetch_template_data(renderData, function () {
 		res.render(renderFile, renderData);
 		if (req.session.end) {
 			req.session.end = null;
 			req.session.destroy();
 		}
-		end();
 	});
 };
 
-exports.login = function (req, res) {
-	prof("login", function(end){
-		if(req.session.admin) {
-			res.redirect('/');
-		} else {
-			res.render("login.html", config.debug ? { debug : true } : null);
+function fetch_template_data (session_data, callback) {
+
+	var count 	= 0,
+		total 	= 1,
+		done 	= function () {
+			if(++count >= total) {
+				callback();
+			}
 		}
-		end();
+
+	about.load(function (data) {
+		session_data.about = data;
+		done();
 	});
 }
 
+exports.login = function (req, res) {
+	if(req.session.admin) {
+		res.redirect('/');
+	} else {
+		res.render("login.html", config.debug ? { debug : true } : null);
+	}
+}
+
 exports.do_login = function (req, res) {
-	prof("do_login", function(end){
-		users.login(req.body, function (login_res) {
-			if(login_res.status) {
-				req.session.username = req.body.username;
-				req.session.admin = true;
-			}
-			res.send(login_res);
-			end();
-		});
+	users.login(req.body, function (login_res) {
+		if(login_res.status) {
+			req.session.username = req.body.username;
+			req.session.admin = true;
+		}
+		res.send(login_res);
 	});
 }
 
 exports.do_logout = function (req, res) {
-	prof("do_logout", function(end){
-		req.session.destroy();
-		req.session = null;
-		res.redirect('/');
-		end();
-	});
+	req.session.destroy();
+	req.session = null;
+	res.redirect('/');
 }
 
 exports.list_images = function (req, res) {
-	prof("list_images", function(end){
-		if(req.session.admin) {
-			images.list_images(function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		images.list_images(function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.load_image = function (req, res) {
-	prof("load_image", function(end){
-		images.load_image({ id : req.params.id }, function (data) {
-			var width 	= parseInt(req.query.width),
-				height 	= parseInt(req.query.height);
-			prof("load_image_sharp", function (end2) {
-				width = isNaN(width) ? null : width;
-				height = isNaN(height) ? null : height;
-				sharp(data.buffer)
-				.resize(width, height)
-	  			.toBuffer(function(err, buffer) {
-	  				end2();
-					res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
-					res.end(buffer, 'binary');
-					end();
-	  			});
-	  		})
-		});
+	images.load_image({ id : req.params.id }, function (data) {
+		var width 	= parseInt(req.query.width),
+			height 	= parseInt(req.query.height);
+			width = isNaN(width) ? null : width;
+			height = isNaN(height) ? null : height;
+		sharp(data.buffer)
+		.resize(width, height)
+			.toBuffer(function(err, buffer) {
+			res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
+			res.end(buffer, 'binary');
+			});
 	});
 }
 
 exports.load_small_image = function (req, res) {
-	prof("load_small_image", function(end){
-		images.load_image({ id : req.params.id }, function (data) {
-			prof("load_small_image_sharp", function(end2) {
-				sharp(data.buffer)
-				.resize(null, 600)
-	  			.toBuffer(function(err, buffer) {
-	  				end2();
-					res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
-					res.end(buffer, 'binary');
-					end();
-	  			});
-	  		})
+
+	images.load_image({ id : req.params.id }, function (data) {
+		sharp(data.buffer)
+		.resize(null, 600)
+			.toBuffer(function(err, buffer) {
+			res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
+			res.end(buffer, 'binary');
 		});
 	});
 }
 
 exports.load_thumb_image = function (req, res) {
-	prof("load_thumb_image", function(end){
-		images.load_image({ id : req.params.id }, function (data) {
-			prof("load_thumb_image_sharp", function (end2) {
-				sharp(data.buffer)
-				.resize(null, 300)
-	  			.toBuffer(function(err, buffer) {
-	  				end2();
-					res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
-					res.end(buffer, 'binary');
-					end();
-	  			});
-	  		});
-		});
+	images.load_image({ id : req.params.id }, function (data) {
+		sharp(data.buffer)
+		.resize(null, 300)
+			.toBuffer(function(err, buffer) {
+			res.writeHead(200, {'Content-Type': data.mimetype, "Cache-Control" : "no-transform,public,max-age=86400" });
+			res.end(buffer, 'binary');
+			});
 	});
 }
 
 
 exports.save_image = function (req, res) {
-	prof("save_image", function(end){
-		if(req.session.admin) {
-			images.save_image({ file: req.files.file }, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		images.save_image({ file: req.files.file }, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.delete_image = function (req, res) {
-	prof("delete_image", function(end){
-		if(req.session.admin) {
-			images.delete_image(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		images.delete_image(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 
 exports.save_about = function (req, res) {
-	prof("save_about", function(end){
-		if(req.session.admin) {
-			about.save(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		about.save(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.load_about = function (req, res) {
-	prof("load_about", function(end){
-		about.load(function (data) {
-			res.send(data);
-			end();
-		});
+	about.load(function (data) {
+		res.send(data);
 	});
 }
 
 
 exports.list_shop_item = function (req, res) {
-	prof("list_shop_item", function(end){
-		shop.list(req.body, function (data) {
-			if(req.session.admin) {
-				shop.access_count(function (counts) {
-					if(data.status && counts.status) {
-						for(var i = 0; i < data.items.length; ++i) {
-							data.items[i].counts = counts.result[data.items[i]._id];
-						}
+	shop.list(req.body, function (data) {
+		if(req.session.admin) {
+			shop.access_count(function (counts) {
+				if(data.status && counts.status) {
+					for(var i = 0; i < data.items.length; ++i) {
+						data.items[i].counts = counts.result[data.items[i]._id];
 					}
-					res.send(data);
-					end();
-				});
-			} else {
+				}
 				res.send(data);
-				end();
-			}
-		});
+			});
+		} else {
+			res.send(data);
+		}
 	});
 }
 
 exports.categories = function (req, res) {
-	prof("categories", function(end){
-		shop.categories(req.body, function (data) {
-			res.send(data);
-			end();
-		});
+	shop.categories(req.body, function (data) {
+		res.send(data);
 	});
 }
 
 exports.save_category = function (req, res) {
-	prof("save_category", function(end){
-		if(req.session.admin) {
-			shop.create_category(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		shop.create_category(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.delete_category = function (req, res) {
-	prof("delete_category", function(end){
-		if(req.session.admin) {
-			shop.delete_category(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		shop.delete_category(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.save_shop_item = function (req, res) {
-	prof("save_shop_item", function(end){
-		if(req.session.admin) {
-			shop.save(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		shop.save(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.load_shop_item = function (req, res) {
-	prof("load_shop_item", function(end){
-		shop.load(req.body, function (data) {
-			if(!req.session.admin && data.status) {
-				var other = req.body;
-				other.address = req.connection.remoteAddress;
-				shop.access(other);
-			}
-			res.send(data);
-			end();
-		});
+	shop.load(req.body, function (data) {
+		if(!req.session.admin && data.status) {
+			var other = req.body;
+			other.address = req.connection.remoteAddress;
+			shop.access(other);
+		}
+		res.send(data);
 	});
 }
 
 exports.delete_shop_item = function (req, res) {
-	prof("delete_shop_item", function(end){
-		if(req.session.admin) {
-			shop.delete(req.body, function (data) {
-				res.send(data);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}	
-	});
+	if(req.session.admin) {
+		shop.delete(req.body, function (data) {
+			res.send(data);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}	
 }
 
 exports.create_order = function (req, res) {
-	prof("create_order", function(end){
-		shop.create_order(req.body, function (data) {
-			res.send(data);
-			end();
-		});
+	shop.create_order(req.body, function (data) {
+		res.send(data);
 	});
 }
 
 exports.load_gallery = function (req, res) {
-	prof("load_gallery", function(end){
-		gallery.load(function (data) {
-			res.send(data);
-			end();
-		});
+	gallery.load(function (data) {
+		res.send(data);
 	});
 }
 
 exports.save_gallery_item = function (req, res) {
-	prof("save_gallery_item", function(end){
-		if(req.session.admin) {
-			images.save_image({ file: req.files.file }, function (data) {
-				
-				if(data.status) {
-					gallery.save(data, function (response) {
-						res.send(response);
-						end();
-					});
-				} else {
-					res.send(data);
-					end();
-				}
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		images.save_image({ file: req.files.file }, function (data) {
+			if(data.status) {
+				gallery.save(data, function (response) {
+					res.send(response);
+				});
+			} else {
+				res.send(data);
+			}
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.delete_gallery_item = function (req, res) {
-	prof("delete_gallery_item", function(end){
-		if(req.session.admin) {
-			images.delete_image(req.body, function (data) {
-				
-				if(data.status) {
-					gallery.delete(req.body, function (response) {
-						res.send(response);
-						end();
-					});
-				} else {
-					res.send(data);
-					end();
-				}
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}
-	});
+	if(req.session.admin) {
+		images.delete_image(req.body, function (data) {
+			if(data.status) {
+				gallery.delete(req.body, function (response) {
+					res.send(response);
+				});
+			} else {
+				res.send(data);
+			}
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}
 }
 
 exports.swap_gallery = function (req, res) {
-	prof("swap_gallery", function(end){
-		if(req.session.admin) {
-			gallery.swap_gallery(req.body, function (response) {
-				res.send(response);
-				end();
-			});
-		} else {
-			res.send({ status: false, error: "Not allowed"});
-			end();
-		}	
-	});
+	if(req.session.admin) {
+		gallery.swap_gallery(req.body, function (response) {
+			res.send(response);
+		});
+	} else {
+		res.send({ status: false, error: "Not allowed"});
+	}	
 }
 
 exports.track_sessions = function (session_store) {
@@ -447,136 +376,100 @@ exports.session_update = function (req, res, next) {
 }
 
 exports.order_add = function (req, res) {
-	prof("order_add", function(end){
-		var data = req.body;
-		data.sessionID = req.sessionID;
-		orders.session_order_add(data, function (result) {
-			
-			res.send(result);
-			end();
-		});
+	var data = req.body;
+	data.sessionID = req.sessionID;
+	orders.session_order_add(data, function (result) {
+		res.send(result);
 	});
 }
 
 exports.order_remove = function (req, res) {
-	prof("order_remove", function(end){
-		var data = req.body;
-		data.sessionID = req.sessionID;
-		orders.session_order_remove(data, function (result) {
-			
-			res.send(result);
-			end();
-		});
+	var data = req.body;
+	data.sessionID = req.sessionID;
+	orders.session_order_remove(data, function (result) {
+		res.send(result);
 	});
 }
 
 exports.order_session = function (req, res) {
-	prof("order_session", function(end){
-		orders.session_order_load(req.sessionID, function (result) {
-			res.send(result);
-			end();
-		});
+	orders.session_order_load(req.sessionID, function (result) {
+		res.send(result);
 	});
 }
 
 
 exports.order_shipping = function (req, res) {
-	prof("order_shipping", function(end){
-		var data = req.body;
-		data.sessionID = req.sessionID;
-		orders.shipping(data, function (result) {
-			res.send(result);
-			end();
-		});
+	var data = req.body;
+	data.sessionID = req.sessionID;
+	orders.shipping(data, function (result) {
+		res.send(result);
 	});
 }
 
 exports.payment_types = function (req, res) {
-	prof("payment_types", function(end){
-		res.send({ 
-			status : true, 
-			types : [
-				"paypal"//, "visa", "mastercard", "discover", "amex"
-			],
-			options: {
-				paypal : {}
-				// card : { expand : true }
-			}
-		});
-		end();
+	res.send({ 
+		status : true, 
+		types : [
+			"paypal"//, "visa", "mastercard", "discover", "amex"
+		],
+		options: {
+			paypal : {}
+			// card : { expand : true }
+		}
 	});
 }
 
 exports.order_pay_paypal = function (req, res) {
-	prof("order_pay_paypal", function(end){
-		var data = req.body || {};
-		data.sessionID = req.sessionID;
-		orders.payment_paypal(data, function (result) {
-			res.send(result);
-			end()
-		});
+	var data = req.body || {};
+	data.sessionID = req.sessionID;
+	orders.payment_paypal(data, function (result) {
+		res.send(result);
 	});
 }
 
 exports.order_pay_card = function (req, res) {
-	prof("order_pay_card", function(end){
-		var data = req.body;
-		data.sessionID = req.sessionID;
-		orders.payment_card(data, function (result) {
-			res.send(result);
-			end();
-		});
+	var data = req.body;
+	data.sessionID = req.sessionID;
+	orders.payment_card(data, function (result) {
+		res.send(result);
 	});
 }
 
 exports.order_process = function (req, res) {
-	prof("order_process", function(end){
-		var data = {
-			sessionID : req.sessionID,
-			accept: config.domain + "/async/paypal/accept",
-			cancel: config.domain + "/async/paypal/cancel"
-		};
-		orders.approve(data, function (result) {
-			if(result.status) {
-				res.redirect(result.link);
-			} else {
-				res.send(result);
-			}
-			end()
-		});
+	var data = {
+		sessionID : req.sessionID,
+		accept: config.domain + "/async/paypal/accept",
+		cancel: config.domain + "/async/paypal/cancel"
+	};
+	orders.approve(data, function (result) {
+		if(result.status) {
+			res.redirect(result.link);
+		} else {
+			res.send(result);
+		}
 	});
 }
 
 exports.paypal_return = function (req, res) {
-	prof("paypal_return", function(end){
-		var data = req.query;
-		data.sessionID = req.sessionID;
-		orders.execute(data, function (result) {
-			req.session.render = "purchase.html";
-			req.session.renderData = result;
-			req.session.end = true;
-			res.redirect("/");
-			end();
-		});
+	var data = req.query;
+	data.sessionID = req.sessionID;
+	orders.execute(data, function (result) {
+		req.session.render = "purchase.html";
+		req.session.renderData = result;
+		req.session.end = true;
+		res.redirect("/");
 	});
 }
 
 exports.paypal_cancel = function (req, res) {
-	prof("paypal_cancel", function(){
-		req.session.render = "purchase.html";
-		res.redirect("/");
-		end();
-	});
-	
+	req.session.render = "purchase.html";
+	res.redirect("/");
 }
 
 exports.contact = function (req, res) {
-	prof("contact", function(end){
-		var data = req.body;
-		data.sessionID = req.sessionID;
-		contact.send(data, function (result) {
-			res.send(result);
-			end();
-		});
+	var data = req.body;
+	data.sessionID = req.sessionID;
+	contact.send(data, function (result) {
+		res.send(result);
 	});
 }
